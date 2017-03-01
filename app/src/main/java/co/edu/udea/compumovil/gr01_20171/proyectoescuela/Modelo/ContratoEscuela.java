421de4ab68a21ab9a11b5007826b062175c366f0
David Alejandro Marin Alzate <jandro240@gmail.com>
2017-03-01 07:15:53 -0500

    agregando proyecto android studio api 16
    * creando la base de datos 50%

---
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Modelo/ContratoEscuela.java
@@ -0,0 +1,124 @@
+package co.edu.udea.compumovil.gr01_20171.proyectoescuela.Modelo;
+
+import java.util.UUID;
+
+/**
+ * Created by Alejandro on 27/02/2017.
+ */
+
+public class ContratoEscuela {
+
+    interface ColumnasEstudiante{
+        String EST_IDENTIFICACION = "est_identificacion";
+        String EST_NOMBRES = "est_nombres";
+        String EST_APELLIDOS = "est_apellidos";
+        String EST_FOTO = "est_foto";
+        String EST_GRP_ID = "est_grp_id";
+        String EST_POS_COL = "est_pos_col";
+        String EST_POS_FILA = "est_pos_fila";
+    }
+
+    interface ColumnasGrupo{
+        String GRP_NOMBRE = "grp_nombre";
+        String GRP_GRADO = "grp_grado";
+    }
+
+    interface ColumnasMaterias{
+        String MTA_ID = "mta_id";
+        String MTA_NOMBRE = "mta_nombre";
+    }
+
+    interface ColumnasMateriasEstudiantes{
+        String MEST_MTA_ID = "mest_mta_id";
+        String MEST_EST_ID = "mest_est_id";
+    }
+
+    interface ColumnasAsistencia{
+        String AST_ASISTENCIA = "ast_asistencia";
+        String AST_FECHA = "ast_fecha";
+        String AST_EST_ID = "ast_est_id";
+    }
+
+    interface ColumnasSeguimiento{
+        String SEG_ID = "seg_id";
+        String SEG_SUBC_ID = "seg_subc_id";
+        String SEG_EST_ID = "seg_est_id";
+        String SEG_ESTADO = "seg_estado";
+        String SEG_FECHA = "seg_fecha";
+        String SEG_TIPO = "seg_tipo";
+    }
+
+    interface ColumnasSubcategorias{
+        String SUBC_ID = "subc_id";
+        String SUBC_CAT_ID = "subc_cat_id";
+        String SUBC_NOMBRE = "subc_nombre";
+        String SUBC_ICONO = "subc_icono";
+    }
+
+    interface ColumnasCategorias{
+        String CAT_ID = "cat_id";
+        String CAT_NOMBRE = "cat_nombre";
+    }
+
+    interface ColumnasMetas{
+
+    }
+
+    interface ColumnasGrupoEstudiantes{
+        String GPEST_ID = "gpest_id";
+        String GPEST_EST_IDENTIFICACION = "";
+    }
+
+    interface ColumnasListaMetas{
+    }
+
+
+    public static class Estudiante implements ColumnasEstudiante{
+        public static String generarIdEstudiante(){
+            return "EST-" + UUID.randomUUID().toString();
+        }
+    }
+
+    public static class Grupo implements ColumnasGrupo{
+        public static String generarIdGrupo(){
+            return "GRP"+ UUID.randomUUID().toString();
+        }
+    }
+
+    public static class Materias implements ColumnasMaterias{
+        public static String generarIdGrupo(){
+            return "MTA"+ UUID.randomUUID().toString();
+        }
+    }
+
+    public static class MateriaEstudiante implements ColumnasMateriasEstudiantes{
+        public static String generarIdGrupo(){
+            return "MEST"+ UUID.randomUUID().toString();
+        }
+    }
+
+    public static class Asistencia implements ColumnasAsistencia{
+        public static String generarIdEstudiante(){
+            return "AST-" + UUID.randomUUID().toString();
+        }
+    }
+
+    public static class Seguimiento implements ColumnasSeguimiento{
+        public static String generarIdEstudiante(){
+            return "SEG-" + UUID.randomUUID().toString();
+        }
+    }
+
+    public static class Subcategorias implements ColumnasSubcategorias{
+        public static String generarIdEstudiante(){
+            return "SUBC-" + UUID.randomUUID().toString();
+        }
+    }
+
+    public static class Categorias implements ColumnasCategorias{
+        public static String generarIdEstudiante(){
+            return "CAT-" + UUID.randomUUID().toString();
+        }
+    }
+
+}
---
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Modelo/ManejaSQL.java
@@ -0,0 +1,55 @@
+package co.edu.udea.compumovil.gr01_20171.proyectoescuela.Modelo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Alejandro on 27/02/2017.
+ */
+
+public class ManejaSQL extends SQLiteOpenHelper {
+
+    private static final String NOMBRE_BASE_DATOS = "escuela.db";
+    private static final int VERSION_ACTUAL = 1;
+    private final Context contexto;
+
+    public ManejaSQL(Context contexto) {
+        super(contexto, NOMBRE_BASE_DATOS, null, VERSION_ACTUAL);
+        this.contexto = contexto;
+    }
+
+    interface Tablas{
+        String TBL_ESTUDIANTE = "tbl_estudiante";
+        String TBL_GRUPO = "tbl_grupo";
+        String TBL_MATERIAS = "tbl_materias";
+        String TBL_MATERIAS_ESTUDIANTE = "tbl_materias_estudiante";
+        String TBL_SEGUIMIENTO = "tbl_seguimiento";
+        String TBL_SUBCATEGORIAS = "tbl_subcategorias";
+        String TBL_CATEGORIAS = "tbl_categorias";
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                db.setForeignKeyConstraintsEnabled(true);
+            } else {
+                db.execSQL("PRAGMA foreign_keys=ON");
+            }
+        }
+    }
+}
