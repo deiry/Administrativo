e405729bada699223008de5a1829de20a3170e49
David Alejandro Marin Alzate <jandro240@gmail.com>
2017-03-08 11:36:03 -0500

    implementando la vistra principal

--- app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/UbicacionFragment.java
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/UbicacionFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 
 /**
@@ -24,6 +26,8 @@ public class UbicacionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ubicacion, container, false);
+
     }
+
 
 }
--- app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Vista/PantallaPpal.java
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Vista/PantallaPpal.java
@@ -3,10 +3,12 @@ package co.edu.udea.compumovil.gr01_20171.proyectoescuela.Vista;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 
 import co.edu.udea.compumovil.gr01_20171.proyectoescuela.R;
+import co.edu.udea.compumovil.gr01_20171.proyectoescuela.UbicacionFragment;
 
 public class PantallaPpal extends AppCompatActivity {
 
@@ -16,8 +18,26 @@ public class PantallaPpal extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_ppal);
 
         agregarFragmentNombreGrupo();
+        agregarFragmentUbicacion();
+        CrearGridView();
     }
 
+    /**
+     * este metodo asigna al contenerdor de ubicacion el fragment de ubicacion
+     */
+    private void agregarFragmentUbicacion() {
+        /*FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        UbicacionFragment fragment = new UbicacionFragment();
+
+        fragmentTransaction.add(R.id.fragmet_contenedor_ubicacion, fragment);
+
+        fragmentTransaction.commit();*/
+    }
+
+    /**
+     * esta funcion asigna el fragment del titulo al lado de las operaciones principales
+     */
     private void agregarFragmentNombreGrupo() {
 
 
@@ -28,5 +48,26 @@ public class PantallaPpal extends AppCompatActivity {
         fragmentTransaction.add(R.id.fragment_contenedor, fragment);
 
         fragmentTransaction.commit();
+    }
+
+
+    private void CrearGridView() {
+        String[] estudiantes = new String[]{"Estudiante1","Estudiante2","Estudiante1","Estudiante2",
+                "Estudiante1","Estudiante2","Estudiante1","Estudiante2",
+                "Estudiante1","Estudiante2","Estudiante1","Estudiante2",
+                "Estudiante1","Estudiante2","Estudiante1","Estudiante2"};
+
+        ArrayAdapter<String> adapter;
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,estudiantes);
+       // adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.item_estudiante,estudiantes);
+
+        GridView gridEstudiante = (GridView) findViewById(R.id.grid_view_ubicacion);
+
+        gridEstudiante.setAdapter(adapter);
+
+        gridEstudiante.setNumColumns(6);
+
+
     }
 }
