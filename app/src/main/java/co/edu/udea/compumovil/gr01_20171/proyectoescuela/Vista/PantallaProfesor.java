fbd5f51dbb009ae2cbd0b40b4439ba8b5c138388
Estefany <estefa942@gmail.com>
2017-03-11 18:10:43 -0500

    dialog guarda grupos
    Agregué el dialog para guardar los grupos

--- app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Vista/PantallaProfesor.java
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Vista/PantallaProfesor.java
@@ -3,8 +3,13 @@ package co.edu.udea.compumovil.gr01_20171.proyectoescuela.Vista;
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import co.edu.udea.compumovil.gr01_20171.proyectoescuela.R;
 
@@ -17,26 +22,43 @@ public class PantallaProfesor extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_profesor);
-        //modifica el custom dialog a mostrar mediante el boton
-        Button agregarGrup = (Button) findViewById(R.id.btn_agregarGrupo);
-        agregarGrup.setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton mostrar = (FloatingActionButton) findViewById(R.id.btn_agregarGrupo);
+        mostrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Show the custom
-                showDialog();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(PantallaProfesor.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_agregar_grupo, null);
+                FloatingActionButton subirEstudiantes = (FloatingActionButton) mView.findViewById(R.id.btn_subirEstudiantes);
+                Button guardar = (Button) mView.findViewById(R.id.btn_guardarGrupo);
+                final EditText numGrado = (EditText) mView.findViewById(R.id.numGrado);
+                final EditText identGrupo = (EditText) mView.findViewById(R.id.idenGrupo);
+               //Subir Estudiantes
+                subirEstudiantes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Colocar condiciones
+                        Toast.makeText(PantallaProfesor.this, R.string.estudiantes_guardadps_msg,Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                guardar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!numGrado.getText().toString().isEmpty() && !identGrupo.getText().toString().isEmpty()){
+                        Toast.makeText(PantallaProfesor.this, R.string.grupo_guardado_msg,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
             }
         });
-    }
+            }
 
-    private void showDialog() {
-        final Dialog addGrupo = new Dialog(this);
-        //set title
-        addGrupo.setTitle("Agregar grupo");
-        //inflate layout
-        addGrupo.setContentView(R.layout.activity_agregar_grupo_alert);
 
-        // Button guardar = (Button)addGrupo.findViewBy(R.id.btn_guardarGrupo);    }
-        addGrupo.show();
-    }
+
+
 
 }
