67188441b9865cb2e19b64366b1b4a13b8685d4a
Andres Moreno <andres.morenog@udea.edu.co>
2017-03-14 08:44:54 -0500

    integracion

---
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20171/proyectoescuela/Vista/DialogAgregarGrupos.java
@@ -0,0 +1,15 @@
+package co.edu.udea.compumovil.gr01_20171.proyectoescuela.Vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.edu.udea.compumovil.gr01_20171.proyectoescuela.R;
+
+public class DialogAgregarGrupos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_agregar_grupos);
+    }
+}
